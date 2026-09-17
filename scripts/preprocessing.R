38b1302c0da201eed0c3887aea9e09ceb322bf69
ncbormann <ncb13@gmx.net>
2026-09-17 16:50:41 +0200

    upload updated cain map app with report download functionality and without incomplete data for Spain, Portugal, and Netherlands

--- scripts/preprocessing.R
+++ scripts/preprocessing.R
@@ -1,14 +1,16 @@
 pacman::p_load(tidyverse, geojsonsf, stringr, lubridate, sf)
 setwd(dirname(getwd())) 
-#setwd("/home/ncb/Dropbox/Documents/ERC_DANGER/website/cain-map-app/scripts")
+setwd("/home/ncb/Dropbox/Documents/ERC_DANGER/website/cain-map-app/")
 
 # europe <- read_csv("../src/lib/data/server_clean_241007.csv") %>%
 #   mutate(date_str = paste0(day_start,str_extract(month, "[a-zA-Z]+"),year),
 #          date = dmy(date_str)) 
 
-europe <- readRDS("../src/lib/data/server_clean_260116.rds") %>%
+europe <- readRDS("../cain_map_input_files/server_clean_260116.rds") %>%
   mutate(date = date_start) 
 
+
+# when I run this, 1504 fail to parse - probably because I'm pulling from two different sets of date columns.
 
 
 nrow(europe %>% filter(is.na(date_start)))
@@ -16,7 +18,7 @@ nrow(europe %>% filter(is.na(date_start)))
 nrow(europe %>% filter(is.na(month)))
 date_test <- europe %>% filter(is.na(month))
 
-actors_clean <- read_csv("../src/lib/data/all_actors_simpliefied_website_2521204.csv")
+actors_clean <- read_csv("../cain_map_input_files/all_actors_simpliefied_website_2521204.csv")
 
 
 radius <- 0.0005
@@ -28,7 +30,8 @@ all_events_filtered <- europe %>%
   rename(actor_group_b_reduced = actor_group_simplified) %>%
   filter(!is.na(town_ln),
          !is.na(town_lat),
-         !is.na(date)) %>%
+         !is.na(date),
+         !country_coded%in%c("Spain", "Portugal", "Netherlands")) %>%
   select(date, country_coded, town_ln, town_lat, actor_a, actor_b, actor_group_a_reduced, actor_group_b_reduced, deaths) %>%
   group_by(town_lat, town_ln) %>%
   mutate(
@@ -57,8 +60,6 @@ nrow(all_events_filtered)
 #   ) %>%
 #   ungroup()
 
-#NOTE: the function directly above gives you bounding boxes containing all the points in each country group. But because there are some outliers (like the one in Australia!) the script uses the following function, which removes the highest and lowest values to remove the miscoded points. Once points have been confirmed this can be switched back.
-
 country_bounds <- all_events_filtered %>%
   group_by(country_coded) %>%
   reframe(
@@ -81,17 +82,18 @@ country_bounds <- all_events_filtered %>%
     center_lat = (min_lat + max_lat) / 2
   ) %>%
   ungroup() %>%
+  # Remove the temporary sorted columns
   select(-sorted_lons, -sorted_lats)
 
 # Save as JSON
-jsonlite::write_json(country_bounds, "../src/lib/data/country_bounds.json", pretty = TRUE)
+jsonlite::write_json(country_bounds, "src/lib/data/country_bounds.json", pretty = TRUE)
 
 
 europe_geojson <- df_geojson(df = all_events_filtered, lon = "lon_new" , lat = "lat_new")
-
+#country_bounds_json 
 
 # Set working directory to file location to write directly to app data 
-write_file(europe_geojson, "../src/lib/data/all_events_no_nulls.json")
+write_file(europe_geojson, "src/lib/data/all_events_no_nulls.json")
 
 
 europe_1925_countries <- c(
@@ -131,9 +133,16 @@ europe_1925_countries <- c(
   "Iceland"
 )
 
-country_borders <- read_csv("../src/lib/data/CShapes-2.0.csv") %>%
+country_borders <- read_csv("../lib/data/CShapes-2.0.csv") %>%
   mutate(name = cntry_name) %>%
   filter(name %in% europe_1925_countries)
+
+
+  
+  
+  # mutate(name = case_when(cntry_name == "Germany (Prussia)" ~ "Germany",
+  #                         cntry_name == "Italy/Sardinia" ~ "Italy",
+  #                         TRUE ~ cntry_name))
 
 countries <- unique(all_events_filtered$country_coded)
 
@@ -146,10 +155,10 @@ nineteentwentyfive <- country_borders %>%
   inner_join(latest_borders) %>%
   filter(gwsyear == max) %>%
   mutate(
-    the_geom = st_as_sfc(the_geom), 
-    cap_geom = st_as_sfc(cap_geom)   
+    the_geom = st_as_sfc(the_geom),  # Convert MULTIPOLYGON WKT to geometry
+    cap_geom = st_as_sfc(cap_geom)   # Convert POINT WKT to geometry
   ) %>%
-  st_set_geometry("the_geom")  
+  st_set_geometry("the_geom")  # Set the_geom as the main geometry column
 
 plot(nineteentwentyfive$the_geom)
 
